888190e574cf5056776570a20f0494957a1e5b71
peel3r <ismc2222@gmail.com>
2017-01-04 16:35:42 +0000

    load price list from json file

---
+++ lib/menu.rb
@@ -0,0 +1,14 @@
+require 'json'
+
+class Menu
+
+  attr_reader :price_list
+
+  def load_price_list
+
+    file = File.read('hipstercoffee.json')
+    @price_list = JSON.parse(file).first['prices'].first
+
+  end
+
+end
